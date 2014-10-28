ef07e757ac1fa1b7e7c052f5a4004f3c7e3de484
Andrew Chavez <adchavez@gmail.com>
2014-10-28 16:34:54 -0500

    Add next/prev paging on restaurants page

--- web/index.php
+++ web/index.php
@@ -15,9 +15,23 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 $app->get('/{id}/', function($id) use($app) {
     $data = file_get_contents(__DIR__."/../data/data.json");
     $data = json_decode($data);
-    $data->resourceurl = $app['resourceurl'];
 
-    return $app['twig']->render('restaurant.twig', (array) $data[$id]);
+    if($id != 0) {
+      $prev = $data->top[$id - 1];
+      $prev->id = $id - 1;
+    }
+
+    if($id != count($data->top) - 1) {
+      $next = $data->top[$id + 1];
+      $next->id = $id + 1;
+    }
+
+    return $app['twig']->render('restaurant.twig', array(
+      'resourceurl' => $app['resourceurl'],
+      'previous' => $prev,
+      'current' => $data->top[$id],
+      'next' => $next
+    ));
 });
 
 $app->get('/', function() use($app) {
