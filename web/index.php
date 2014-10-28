0e138eeb821cb86fd41e3a996d3dcace625532ab
Andrew Chavez <adchavez@gmail.com>
2014-10-28 17:38:50 -0500

    Add a map view with top 25 restaurants

--- web/index.php
+++ web/index.php
@@ -12,6 +12,16 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
+$app->get('/map/', function() use($app) {
+  $data = file_get_contents(__DIR__."/../data/data.json");
+  $data = json_decode($data);
+
+  return $app['twig']->render('map.twig', array(
+    'resourceurl' => $app['resourceurl'],
+    'locations' => (array) $data->top
+  ));
+});
+
 $app->get('/{id}/', function($id) use($app) {
     $data = file_get_contents(__DIR__."/../data/data.json");
     $data = json_decode($data);
@@ -20,8 +30,7 @@ $app->get('/{id}/', function($id) use($app) {
       $prev = $data->top[$id - 1];
       $prev->id = $id - 1;
     }
-
-    if($id != count($data->top) - 1) {
+    elseif($id != count($data->top) - 1) {
       $next = $data->top[$id + 1];
       $next->id = $id + 1;
     }
