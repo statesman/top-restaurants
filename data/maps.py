069496e4a3b9d6c2e595d5131d62292e0a6cd674
Andrew Chavez <adchavez@gmail.com>
2014-10-31 10:59:00 -0500

    Fixes #4. Add back locator maps, served statically from assets/ folder

--- data/maps.py
+++ data/maps.py
@@ -6,21 +6,52 @@ j = json.load(f)
 marker_base = 'http://projects.statesman.com/features/top-restaurants/assets/markers/'
 
 map_base = 'https://maps.googleapis.com/maps/api/staticmap?'
-map_params = {}
+map_file_dest = '../assets/maps/'
+map_params_default = {
+  'zoom': '12',
+  'size': '300x250',
+  'maptype': 'terrain'
+}
 
-for i, restaurant in enumerate(j['top']):
-  print "Downloading map for " + restaurant['name']
-
-  latlong = str(restaurant['lat']) + ',' + str(restaurant['long'])
-  map_params['center'] = latlong
-  map_params['zoom'] = '12'
-  map_params['size'] = '325x225'
-  map_params['maptype'] = 'terrain'
-  map_params['markers'] = 'icon:' + marker_base + str(restaurant['position']) + '.png' + '|' + latlong
-
-  r = requests.get(map_base + urllib.urlencode(map_params), stream=True)
-  path = '../assets/maps/' + str(i) + '.jpg'
+def save_map(url, file_name):
+  """
+  """
+  r = requests.get(url, stream=True)
+  path = map_file_dest + str(file_name) + '.jpg'
   if r.status_code == 200:
       with open(path, 'wb') as f:
           for chunk in r.iter_content():
               f.write(chunk)
+
+def marker_string(position, latlong):
+  """
+  """
+  return 'icon:' + marker_base + str(position) + '.png' + '|' + latlong
+
+# Loop through the full set of top restaurants
+for i, restaurant in enumerate(j['top']):
+  print "Downloading map for " + restaurant['name']
+
+  latlong = str(restaurant['lat']) + ',' + str(restaurant['long'])
+
+  map_params = map_params_default
+  map_params['center'] = latlong
+  map_params['markers'] = marker_string(restaurant['position'], latlong)
+
+  save_map(map_base + urllib.urlencode(map_params), i)
+
+# Then save a static map for the combined Uchi/Uchiko listing
+print "Downloading a special map for Uchis."
+
+map_params = map_params_default
+
+uchi = j['top'][2]
+uchiko = j['top'][3]
+
+latlong_uchi = str(uchi['lat']) + ',' + str(uchi['long'])
+latlong_uchiko = str(uchiko['lat']) + ',' + str(uchiko['long'])
+
+map_params['center'] = str((uchi['lat'] + uchiko['lat']) / 2) + 'x' + str((uchi['long'] + uchiko['long']) / 2)
+map_params['size'] = '300x325'
+map_params['markers'] = marker_string(3, latlong_uchi)
+save_map(map_base + urllib.urlencode(map_params) + '&markers=' + urllib.quote_plus(marker_string(3, latlong_uchiko)), 'uchis')
